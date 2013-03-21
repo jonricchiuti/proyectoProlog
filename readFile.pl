29b851071680e7b33ccca8c8bf5d5dc55a439f37
Sofia <sofiabravo103@gmail.com>
2013-03-21 01:32:25 -0430

    genera la primera sopa de letras con las palabras en horizontal

--- readFile.pl
+++ readFile.pl
@@ -1,31 +1,93 @@
 cargarListaPalabra(Alfabeto,Archivo) :-
-  open(Archivo,read,Str),
-  read(Str,List),
-  close(Str),
-  verificar(List,Alfabeto).
+	open(Archivo,read,Str),
+	read(Str,List),
+	close(Str),
+	verificar(List,Alfabeto),
+	generarHechos(Alfabeto),
+	palabrasAceptadas(List,Aceptadas),
+	hola(Tablero,5,Aceptadas).
+
+palabrasAceptadas([],_).
+
+palabrasAceptadas([H|T],[H_|T_]) :-
+	atom_chars(H,H_),
+	palabrasAceptadas(T,T_).
+
+generarHechos([]).
+
+generarHechos([H|T]) :-
+	assert(letra(H)),
+	generarHechos(T).
 
 verificar([],_).
 
 verificar([H|T],List) :-
-  atom_chars(H,X),
-  puedeFormarse(X,List),
-  verificar(T,List).
+	atom_chars(H,X),
+	puedeFormarse(X,List),
+	verificar(T,List).
 
 puedeFormarse([],_).
 
 puedeFormarse([H|T],List) :-
-  member(H,List),
-  puedeFormarse(T,List).
+	member(H,List),
+	puedeFormarse(T,List).
 
-holaSusana([],_).
+crearColumna(X,Tablero) :- 
+	length(Tablero,X),
+	rellenarColumna(Tablero).
 
-holaSusana([X|T],Z) :-
-  length(X,5),
-  holaSusana(T,Z).
+crearTablero([],_).
 
-crearTablero(0,[]).
+crearTablero([H|T],X) :- 
+	crearColumna(X,H),
+	crearTablero(T,X).
 
-crearTablero(X,Tablero) :- 
-  length(Tablero,X),
-  holaSusana(Tablero,X).
-  
+hola(Lista,X,Aceptadas) :-
+	length(Lista,X),
+	crearTablero(Lista,X),
+	epa(Lista,Aceptadas),
+	mostrarSopa(Lista).
+
+
+holis([H|T],Palabra) :-
+	ver_horizontal(H,Palabra);
+	holis(T,Palabra).
+
+epa(_,[]).
+
+epa(Lista,[H|T]) :-
+	holis(Lista,H),
+	epa(Lista,T).
+	
+
+rellenarColumna([]).
+
+rellenarColumna([H|T]) :-
+	letra(H),
+	rellenarColumna(T).
+
+mostrarLista([]).
+
+mostrarLista([H|T]) :-
+	write(H),
+	write(' '),
+	mostrarLista(T).
+
+mostrarSopa([]).
+
+mostrarSopa([H|T]) :-
+	mostrarLista(H),
+	write('\n'),
+	mostrarSopa(T).
+	
+
+ver_horizontal([],[]).
+ver_horizontal([X|Conjunto_tail], [X|Sub_tail]) :-
+	ver_horizontal_esp(Conjunto_tail,Sub_tail).
+ver_horizontal([_|Conjunto_tail], Sub) :-
+	ver_horizontal(Conjunto_tail,Sub).
+
+ver_horizontal_esp(_,[]).
+ver_horizontal_esp([X|Conjunto_tail], [X|Sub_tail]) :-
+	ver_horizontal_esp(Conjunto_tail,Sub_tail).
+
